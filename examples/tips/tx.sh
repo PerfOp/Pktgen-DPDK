21947719583f7f0b122a2c829cdb78013425ac85
huajianwang <huajianwang@gmail.com>
2025-09-16 08:10:41 +0000

    MOD: Update for tx and capture

--- examples/tips/tx.sh
+++ examples/tips/tx.sh
@@ -13,13 +13,22 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
-pci=$(func_get_pci $1);
-echo $pci
+PCI=$(func_get_pci $1);
 
-# 2 tx end
-# # $pmd -l 0-3 -n 4 -a $pci \
-# #	-- --port-topology=chained \
-# #	--nb-cores=3 \
-# #	--forward-mode=txonly \
-# #	--eth-peer=0,7c:1e:52:08:d9:22 \
-# #	--stats-period 1
+echo $PCI
+EAL_ARGS="-l 0-3 -n 4 -a $PCI"
+
+## 1 interaction mode for dpdk-pmd
+#$pmd $EAL_ARGS -- -i --port-topology=chained
+
+# 2 txonly for send
+# Input "set 0 count 0 \n start 0 " to start send
+# Capture: dpdk-dumpcap -c 100 -w /tmp/capture.pcapng
+$pmd $EAL_ARGS \
+    -- -i --forward-mode=txonly \
+    --port-topology=chained --txpkts=64 --stats-period=1
+#\
+#--nb-cores=3 \
+#--forward-mode=txonly \
+#--eth-peer=0,7c:1e:52:08:d9:22 \
+#--stats-period 1
