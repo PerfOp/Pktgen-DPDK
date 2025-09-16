d094b8d7684587215c226e651a713acd5de30039
huajianwang <huajianwang@gmail.com>
2025-09-16 11:31:03 +0000

    MOD: Update with forward mode

---
+++ examples/tips/fwd.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+##############################################################
+# File Name: fwd.sh
+# Author:
+# mail:
+# Created Time: Mon Sep 15 13:37:09 2025
+##############################################################
+
+source ./hosts.sh
+source ./utils.sh
+
+if [ -z "$1" ]; then
+  echo "Please input NIC name, such as eth1..."
+  exit 1
+fi
+
+PCI=$(func_get_pci $1);
+
+echo $PCI
+EAL_ARGS="-l 0-3 -n 4 -a $PCI"
+
+## 1 interaction mode for dpdk-pmd
+#$pmd $EAL_ARGS -- -i --port-topology=chained
+
+# 2 txonly for send
+# * Capture: dpdk-dumpcap -c 100 -w capture.pcapng
+# * Check:   tcpdump -nnn -e -r capture.pcapng
+# https://doc.dpdk.org/guides/testpmd_app_ug/run_app.html#testpmd-command-line-options
+$pmd $EAL_ARGS \
+    -- -i --forward-mode=macswap --stats-period=1 --auto-start
+    #--nb-cores=3 --rxq=4 --txq=4 --rxd=1024 --txd=1024 --mbcache=512 \
